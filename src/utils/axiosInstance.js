43d886574765b4ba7bf8694fa7af23afacbe0186
ShahabImtiaz <shahabimtiaz03@gmail.com>
2023-06-17 17:35:32 +0500

    update

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://3.133.0.29', // Replace with your API base URL
-  timeout: 5000, // Set the timeout value as per your requirement
+  baseURL: 'http://3.133.0.29',
+  timeout: 5000, 
   headers: {
     'Content-Type': 'application/json',
-    // Add any additional headers you need
   },
 });
 
